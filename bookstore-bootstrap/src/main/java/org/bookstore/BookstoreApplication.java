58426b7cc6e431219e1af584dbcc1ef5887bad9c
VanNguyen2804 <dinhvan2804@gmail.com>
2025-06-01 21:49:04 +0700

    fix failed build

--- bookstore-bootstrap/src/main/java/org/bookstore/BookstoreApplication.java
+++ bookstore-bootstrap/src/main/java/org/bookstore/BookstoreApplication.java
@@ -2,6 +2,7 @@ package org.bookstore;
 
 import org.bookstore.config.BookstoreApplicationConfiguration;
 import org.bookstore.config.BookstoreDomainConfiguration;
+import org.bookstore.config.WebSecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -10,7 +11,8 @@ import org.springframework.context.annotation.Import;
 @SpringBootApplication
 @Import({
         BookstoreDomainConfiguration.class,
-        BookstoreApplicationConfiguration.class
+        BookstoreApplicationConfiguration.class,
+        WebSecurityConfig.class
 })
 public class BookstoreApplication {
     public static void main(String[] args) {
